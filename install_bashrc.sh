96d69e5aadfa4cbd1196d2f29d264da69f9c113c
Torsten Scholer <hottehead@gmail.com>
2014-09-19 21:13:45 +0200

    Initial status of my bashrc configuration

---
+++ install_bashrc.sh
@@ -0,0 +1,4 @@
+cd ~/.bash_runtime
+echo '# DO NOT EDIT THIS LINE. It is added by bashrc configuration
+source ~/.bash_runtime/bashrc' >> ~/.profile
+echo "Installed bashrc. Appendend line to ~/.profile file. Enjoy =)"
