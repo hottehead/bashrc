872921afa0d6b8e0c2066af3ece8a3c05ce6c720
Torsten Scholer <hottehead@gmail.com>
2014-12-10 15:19:15 +0100

    Initial Linux setup

---
+++ install_bashrc.sh
@@ -0,0 +1,4 @@
+cd ~/.bash_runtime
+echo '# DO NOT EDIT THIS LINE. It is added by bashrc configuration
+source ~/.bash_runtime/bashrc' >> ~/.bashrc
+echo "Installed bashrc. Appendend line to ~/.bashrc file. Enjoy =)"
